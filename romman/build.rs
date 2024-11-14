e84fcb4038f867cc7111910839915421054f16ef
Jayme Davis <jaymedavis@gmail.com>
2024-11-13 23:14:30 -0500

    added ico

--- romman/build.rs
+++ romman/build.rs
@@ -1,3 +1,13 @@
+use std::env;
+
+extern crate winresource;
+
 fn main() {
     slint_build::compile("ui/main-window.slint").unwrap();
+
+    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        winresource::WindowsResource::new()
+            .set_icon("assets/logo.ico")
+            .compile().unwrap();
+    }
 }
